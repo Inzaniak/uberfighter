08e09e462ff619ba3c9469653fa6e49b0276f96e
Umberto <inzaniak@gmail.com>
2018-02-14 18:01:46 +0100

    Added Expansion on cards (table and hand) + meme expansion

--- decksmanager.py
+++ decksmanager.py
@@ -6,8 +6,9 @@ def import_deck(path):
     conn = sqlite3.connect('data/db.db')
     conn.execute('delete from Decks where Expansion = ?',(deck['Expansion'],))
     for c in deck['Cards']:
+        print(c)
         conn.execute('insert into Decks (Expansion,Type,Description) VALUES (?,?,?)',(deck['Expansion'],c['Type'],c['Description']))
     conn.commit()
     print('Deck Added')
 
-import_deck('data/decks/superfight.json')
+import_deck('data/decks/meme.json')
